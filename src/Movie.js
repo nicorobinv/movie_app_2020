6c9ff03a994def9977fe6e41108699d99709d224
nicorobinv <nicorobinv@gmail.com>
2020-10-18 20:03:25 +0900

    Update Movie.js
    fixed

--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./App.css";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
           {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
+            <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary.slice(0, 140)}...</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
